f487a3ad8575edab6f5047f3e07d538b437dbcb9
Gabriel Policiuc <gabriel.policiuc@gmail.com>
2020-03-20 08:04:37 +0200

    Added base64 prefix check/append if needed Try catch code to prevent app failures

--- ios/Grayscale.m
+++ ios/Grayscale.m
@@ -5,44 +5,57 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(toGrayscale:(NSString *)base64 callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(toGrayscale:(NSString *)base64 addDataImagePrefix:(BOOL)addDataImagePrefix callback:(RCTResponseSenderBlock)callback)
 {
-    NSURL *url = [NSURL URLWithString:base64];
-    NSData *imageData = [NSData dataWithContentsOfURL:url];
-    UIImage *originalImage = [UIImage imageWithData:imageData];
-    UIImage* grayscaleImage = [self convertImageToGrayScale: originalImage];
-    NSString* base64String = [UIImagePNGRepresentation(grayscaleImage) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-    callback(@[base64String]);
+    @try {
+        if ([base64 hasPrefix:@"data:image/"] == NO) {
+            base64 = [@"data:image/png;base64," stringByAppendingString:base64];
+        }
+        NSURL *url = [NSURL URLWithString:base64];
+        NSData *originalImageData = [NSData dataWithContentsOfURL:url];
+        UIImage *originalImage = [UIImage imageWithData:originalImageData];
+        UIImage* grayscaleImage = [self convertImageToGrayscale: originalImage];
+        NSString* base64Grayscale = [UIImagePNGRepresentation(grayscaleImage) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+        if (addDataImagePrefix == NO) {
+            callback(@[base64Grayscale]);
+        }
+        else {
+            callback(@[[@"data:image/png;base64," stringByAppendingString:base64Grayscale]]);
+        }
+    }
+    @catch(NSException* exception) {
+        callback(@[@""]);
+    }
 }
 
-- (UIImage *)convertImageToGrayScale:(UIImage *)image
+- (UIImage *)convertImageToGrayscale:(UIImage *)image
 {
-  // Create image rectangle with current image width/height
-  CGRect imageRect = CGRectMake(0, 0, image.size.width, image.size.height);
-
-  // Grayscale color space
-  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
-
-  // Create bitmap content with current image size and grayscale colorspace
-  CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone);
-
-  // Draw image into current context, with specified rectangle
-  // using previously defined context (with grayscale colorspace)
-  CGContextDrawImage(context, imageRect, [image CGImage]);
-
-  // Create bitmap image info from pixel data in current context
-  CGImageRef imageRef = CGBitmapContextCreateImage(context);
-
-  // Create a new UIImage object
-  UIImage *newImage = [UIImage imageWithCGImage:imageRef];
-
-  // Release colorspace, context and bitmap information
-  CGColorSpaceRelease(colorSpace);
-  CGContextRelease(context);
-  CFRelease(imageRef);
-
-  // Return the new grayscale image
-  return newImage;
+    // Create image rectangle with current image width/height
+    CGRect imageRect = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    // Grayscale color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    
+    // Create bitmap content with current image size and grayscale colorspace
+    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone);
+    
+    // Draw image into current context, with specified rectangle
+    // using previously defined context (with grayscale colorspace)
+    CGContextDrawImage(context, imageRect, [image CGImage]);
+    
+    // Create bitmap image info from pixel data in current context
+    CGImageRef imageRef = CGBitmapContextCreateImage(context);
+    
+    // Create a new UIImage object
+    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+    
+    // Release colorspace, context and bitmap information
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    CFRelease(imageRef);
+    
+    // Return the new grayscale image
+    return newImage;
 }
 
 
